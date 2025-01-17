43f41b7af788275de219253ec7906a6a0fded6ac
Sunday-Okey <sunnybest4500@gmail.com>
2025-01-17 16:17:58 -0500

    added files

--- multconstructors.py
+++ multconstructors.py
@@ -1,5 +1,5 @@
 # https://realpython.com/python-multiple-constructors/
-
+from datetime import date
 # Using Multiple Constructors in Your Python Classes
 # Sometimes you need to write a Python class that provides multiple ways to construct objects. In other words, you want a class that implements multiple constructors. This kind of class comes in handy when you need to create instances using different types or numbers of arguments. Having the tools to provide multiple constructors will help you write flexible classes that can adapt to changing needs.
 # Python offers several techniques and tools that you can use to construct classes, including simulating multiple constructors through optional arguments, customizing instance creation via class methods, and doing special dispatch with decorators.
@@ -11,6 +11,7 @@
 # Object-Oriented Programming
 # Classes - The Blueprint for objects
 # Instances - The individual objects
+
 
 class Person:
     def __init__(self, name):
@@ -112,3 +113,31 @@ cube = CumulativePowerFactory(exponent=3)
 print(cube(21))
 print(cube(42))
 print(cube.total)
+
+initialized_cube = CumulativePowerFactory(3, start=2250)
+print(initialized_cube(21))
+print(initialized_cube(42))
+print(initialized_cube.total)
+
+# Optional Arguments are clean and Pythonic
+
+# Checking Argument Types in __init__()
+
+
+class Person:
+    def __init__(self, name, birth_date):
+        self.name = name
+        if isinstance(birth_date, date):
+            self.birth_date = birth_date
+        elif isinstance(birth_date, str):
+            self.birth_date = date.fromisoformat(birth_date)
+        else:
+            raise ValueError(f"unsupported date format: {birth_date}")
+
+
+jane = Person('John Doe', "2000-11-29")
+print(jane.birth_date)
+john = Person("John Doe", date(1998, 5, 15))
+print(john.birth_date)
+
+# This Technique does not scale well
